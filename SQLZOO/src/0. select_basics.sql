023edbf59d474964544b575bdb7e8db00867a59e
U盘 <624336638@qq.com>
2020-07-04 17:02:17 +0800

    SQLZOO

---
+++ SQLZOO/src/0. select_basics.sql
@@ -0,0 +1,7 @@
+#############################
+# intro: Some simple queries to get you started
+# url: https://sqlzoo.net/wiki/SELECT_basics
+# date:2020-07-04
+# author:shaw
+#############################
+
