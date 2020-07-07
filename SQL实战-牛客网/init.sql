31a05246bfae3e3ce9610f389881ac1a369ac0d3
U盘 <624336638@qq.com>
2020-07-07 23:45:06 +0800

    加入新数据 新表

--- SQL实战-牛客网/init.sql
+++ SQL实战-牛客网/init.sql
@@ -38,6 +38,13 @@ CREATE TABLE `dept_emp` (
 `to_date` date NOT NULL,
 PRIMARY KEY (`emp_no`,`dept_no`));
 
+-- 职称表
+CREATE TABLE IF NOT EXISTS `titles` (
+`emp_no` int(11) NOT NULL,
+`title` varchar(50) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date DEFAULT NULL);
+
 -- 员工数据插入
 
 -- NBA
@@ -130,3 +137,29 @@ INSERT INTO dept_emp VALUES(10008,'d005','1998-03-11','2000-07-31');
 INSERT INTO dept_emp VALUES(10009,'d006','1985-02-18','9999-01-01');
 INSERT INTO dept_emp VALUES(10010,'d005','1996-11-24','2000-06-26');
 INSERT INTO dept_emp VALUES(10010,'d006','2000-06-26','9999-01-01');
+
+
+
+INSERT INTO titles VALUES(10001,'Senior Engineer','1986-06-26','9999-01-01');
+INSERT INTO titles VALUES(10002,'Staff','1996-08-03','9999-01-01');
+INSERT INTO titles VALUES(10003,'Senior Engineer','1995-12-03','9999-01-01');
+INSERT INTO titles VALUES(10004,'Engineer','1986-12-01','1995-12-01');
+INSERT INTO titles VALUES(10004,'Senior Engineer','1995-12-01','9999-01-01');
+INSERT INTO titles VALUES(10005,'Senior Staff','1996-09-12','9999-01-01');
+INSERT INTO titles VALUES(10005,'Staff','1989-09-12','1996-09-12');
+INSERT INTO titles VALUES(10006,'Senior Engineer','1990-08-05','9999-01-01');
+INSERT INTO titles VALUES(10007,'Senior Staff','1996-02-11','9999-01-01');
+INSERT INTO titles VALUES(10007,'Staff','1989-02-10','1996-02-11');
+INSERT INTO titles VALUES(10008,'Assistant Engineer','1998-03-11','2000-07-31');
+INSERT INTO titles VALUES(10009,'Assistant Engineer','1985-02-18','1990-02-18');
+INSERT INTO titles VALUES(10009,'Engineer','1990-02-18','1995-02-18');
+INSERT INTO titles VALUES(10009,'Senior Engineer','1995-02-18','9999-01-01');
+INSERT INTO titles VALUES(10010,'Engineer','1996-11-24','9999-01-01');
+INSERT INTO titles VALUES(10010,'Engineer','1996-11-24','9999-01-01');
+
+INSERT INTO departments VALUES('d001','Marketing');
+INSERT INTO departments VALUES('d002','Finance');
+INSERT INTO departments VALUES('d003','Human Resources');
+INSERT INTO departments VALUES('d004','Production');
+INSERT INTO departments VALUES('d005','Development');
+INSERT INTO departments VALUES('d006','Quality Management');
