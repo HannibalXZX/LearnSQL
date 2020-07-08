873a37f026c0bce548f45cac2c5833cd095ea90a
U盘 <624336638@qq.com>
2020-07-09 00:13:26 +0800

    增加2.sql

---
+++ SQL实战-牛客网/init_2.sql
@@ -0,0 +1,56 @@
+CREATE TABLE IF NOT EXISTS film (
+film_id smallint(5)  NOT NULL DEFAULT '0', -- 电影ID
+title varchar(255) NOT NULL,               -- 电影名称
+description text,                          -- 电影描述信息
+PRIMARY KEY (film_id));
+
+CREATE TABLE category  (
+category_id  tinyint(3)  NOT NULL ,        -- 电影分类ID
+name  varchar(25) NOT NULL,                -- 电影分类名称
+`last_update` timestamp,                   -- 电影分类最后更新时间
+PRIMARY KEY ( category_id ));
+
+CREATE TABLE film_category  (
+film_id  smallint(5)  NOT NULL,            -- 电影ID
+category_id  tinyint(3)  NOT NULL,         -- 电影分类ID
+`last_update` timestamp);                  -- 电影id和分类id对应关系的最后更新时间
+
+
+INSERT INTO film VALUES(1,'ACADEMY DINOSAUR','A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies');
+INSERT INTO film VALUES(2,'ACE GOLDFINGER','A Astounding Epistle of a Database Administrator And a Explorer who must Find a Car in Ancient China');
+INSERT INTO film VALUES(3,'ADAPTATION HOLES','A Astounding Reflection of a Lumberjack And a Car who must Sink a Lumberjack in A Baloon Factory');
+INSERT INTO film VALUES(4,'AFFAIR PREJUDICE','A Fanciful Documentary of a Frisbee And a Lumberjack who must Chase a Monkey in A Shark Tank');
+INSERT INTO film VALUES(5,'AFRICAN EGG','A Fast-Paced Documentary of a Pastry Chef And a Dentist who must Pursue a Forensic Psychologist in The Gulf of Mexico');
+INSERT INTO film VALUES(6,'AGENT TRUMAN','A Intrepid Panorama of a robot And a Boy who must Escape a Sumo Wrestler in Ancient China');
+INSERT INTO film VALUES(7,'AIRPLANE SIERRA','A Touching Saga of a Hunter And a Butler who must Discover a Butler in A Jet Boat');
+INSERT INTO film VALUES(8,'AIRPORT POLLOCK','A Epic Tale of a Moose And a Girl who must Confront a Monkey in Ancient India');
+INSERT INTO film VALUES(9,'ALABAMA DEVIL','A Thoughtful Panorama of a Database Administrator And a Mad Scientist who must Outgun a Mad Scientist in A Jet Boat');
+INSERT INTO film VALUES(10,'ALADDIN CALENDAR','A Action-Packed Tale of a Man And a Lumberjack who must Reach a Feminist in Ancient China');
+
+INSERT INTO category VALUES(1,'Action','2006-02-14 20:46:27');
+INSERT INTO category VALUES(2,'Animation','2006-02-14 20:46:27');
+INSERT INTO category VALUES(3,'Children','2006-02-14 20:46:27');
+INSERT INTO category VALUES(4,'Classics','2006-02-14 20:46:27');
+INSERT INTO category VALUES(5,'Comedy','2006-02-14 20:46:27');
+INSERT INTO category VALUES(6,'Documentary','2006-02-14 20:46:27');
+INSERT INTO category VALUES(7,'Drama','2006-02-14 20:46:27');
+INSERT INTO category VALUES(8,'Family','2006-02-14 20:46:27');
+INSERT INTO category VALUES(9,'Foreign','2006-02-14 20:46:27');
+INSERT INTO category VALUES(10,'Games','2006-02-14 20:46:27');
+INSERT INTO category VALUES(11,'Horror','2006-02-14 20:46:27');
+INSERT INTO category VALUES(12,'Music','2006-02-14 20:46:27');
+INSERT INTO category VALUES(13,'New','2006-02-14 20:46:27');
+INSERT INTO category VALUES(14,'Sci-Fi','2006-02-14 20:46:27');
+INSERT INTO category VALUES(15,'Sports','2006-02-14 20:46:27');
+INSERT INTO category VALUES(16,'Travel','2006-02-14 20:46:27');
+
+INSERT INTO film_category VALUES(1,6,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(2,11,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(3,6,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(4,11,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(5,6,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(6,6,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(7,5,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(8,6,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(9,11,'2006-02-14 21:07:09');
+INSERT INTO film_category VALUES(10,15,'2006-02-14 21:07:09');
